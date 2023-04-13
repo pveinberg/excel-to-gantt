66194583ef0670a9e4e31399d9f2794155f5fa66
Pablo Veinberg <pveinberg@gmail.com>
2023-04-13 07:09:49 -0300

    Update code
    Adjust initial and final time for charts

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from ganttchart import GanttChart
 
-mygantt = GanttChart('project_example_source.xlsx', 'MAIN PROJECT')
+mygantt = GanttChart('conectividade_2023.xlsx', 'CONECTIVIDADE')
 mygantt.load_data_and_holidays()
 
 # test ok
-# mygantt.build_chart()
+mygantt.build_chart()
 
 # test ok
-# mygantt.calculate_diff_and_delay()
+mygantt.calculate_diff_and_delay()
 
-
-fields = ['start_date', 'forecast', 'accomplished']
-for _field in fields:
+# test ok
+for _field in ['start_date', 'forecast', 'accomplished']:
     print(f"Records with date before today, by '{_field}'")
     print(mygantt.records_with_date_before_today_by(_field))
 
